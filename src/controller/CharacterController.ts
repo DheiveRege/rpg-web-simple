b6ef8f8897a91b25512230e2cb15f49786eeabe5
DHEIVE RODRIGUES FUCK <04787810081@senacrs.edu.br>
2026-08-25 21:44:56 -0300

    feat/add controller of Character

---
+++ src/controller/CharacterController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { CharacterService } from '../service/CharacterService';
+
+const characterService = new CharacterService();
+
+export class UserController {
+    async list(req: Request, res: Response) {
+        const chraacter = await characterService.list();
+        return res.json(chraacter);
+    }
+
+    async show(req: Request, res: Response) {
+
+        const chraacter = await characterService.show(Number(req.params.id));
+        return res.json(chraacter);
+
+    }
+
+    async create(req: Request, res: Response) {
+
+        const { name, className } = req.body;
+
+        const character = await characterService.create(name, className);
+
+        return res.status(201).json(character);
+
+    }
+
+
+    async delete(req: Request, res: Response) {
+
+        await characterService.delete(Number(req.params.id));
+
+        return res.status(204).send();
+
+    }
+}
